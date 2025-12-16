9303c12bca5b11e72bfb71653a369ead3e679d99
constanzajugo-create <constanza.jugo@afpcapital.cl>
2025-12-16 10:17:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ import pandas as pd
 
 st.title("Screening Tool")
 
-st.markdown("""
-<style>
-table {
-    width: 100% !important;
-}
-</style>
-""", unsafe_allow_html=True)
-
 
 # --- Cargar CSV ---
 @st.cache_data
@@ -132,6 +124,7 @@ if selected_gp != "Todos":
 
 else:
     st.info("Seleccione un GP para ver el resumen.")
+
 
 
 
